8a9c619f70f233829a7c8f7a6dd58aaca81d6828
Ken Maupin <kenneth.maupin@workday.com>
2013-03-29 18:00:43 -0700

    Checkpoint commit of working but imperfect code.

---
+++ make/common.mk
@@ -0,0 +1,29 @@
+#________________________________________
+#
+#
+
+#
+#
+#
+
+BIN_PATH			= $(TOP_PATH)/bin
+BUILD_PATH			= $(TOP_PATH)/build
+FLAGS_PATH			= $(TOP_PATH)/flags
+INSTALL_BIN_PATH		= $(INSTALL_PATH)/bin
+INSTALL_INCLUDE_PATH		= $(INSTALL_PATH)/include
+INSTALL_LIB_PATH		= $(INSTALL_PATH)/lib
+INSTALL_PATH			= $(TOP_PATH)/install
+MAKE_PATH			= $(TOP_PATH)/make
+SOURCE_PATH			= $(TOP_PATH)/source
+TOP_PATH			= $(shell pwd)
+
+#
+#
+#
+
+CFLAGS				= 
+LDFLAGS				= 
+
+
+
+
